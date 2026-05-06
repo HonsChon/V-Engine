a29f5a1331c2b926b70d9c4786acb754b8334119
HonsChon <191462031@qq.com>
2026-05-06 23:06:17 +0800

    [RHI]DX12

---
+++ src/RHI/DX12/DX12RHIBuffer.cpp
@@ -0,0 +1,9 @@
+#include "DX12RHIBuffer.h"
+
+void *DX12RHIBuffer::map()
+{
+    if (mapped_)
+    {
+        return;
+    }
+}
---
+++ src/RHI/DX12/DX12RHIBuffer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "RHIBuffer.h"
+
+class DX12RHIDevice;
+
+class DX12RHIBuffer : public RHIBuffer
+{
+public:
+    DX12RHIBuffer(DirectXRHIDevice* device, RHIBuffre& desc);
+    ~DX12RHIBuffer() override;
+
+    void* map() override;
+    void* unmap() override;
+    void  uploadData(const void* data, uint64_t size, uint64_t offset = 0) override;
+
+    uint64_t       getSize() const override { return desc_.size; }
+    RHIBufferUsage getUsage() const override { return desc_.usage; }
+    RHIMemoryUsage getMemoryUsage() const override { return desc_.memoryUsage; }
+
+
+
+
+}
